516e6be907b05431d462987694c19fc9f62d9e55
ameyakadam <42661612+ameyakadam@users.noreply.github.com>
2018-10-28 21:48:39 -0600

    Add files via upload

---
+++ Project A and B - KADAM/Kadam_project_3A/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.box1 = new System.Windows.Forms.MaskedTextBox();
+            this.box3 = new System.Windows.Forms.MaskedTextBox();
+            this.box2 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(317, 242);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(536, 242);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // box1
+            // 
+            this.box1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box1.Location = new System.Drawing.Point(536, 49);
+            this.box1.Margin = new System.Windows.Forms.Padding(4);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(165, 27);
+            this.box1.TabIndex = 2;
+            // 
+            // box3
+            // 
+            this.box3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box3.Location = new System.Drawing.Point(536, 167);
+            this.box3.Margin = new System.Windows.Forms.Padding(4);
+            this.box3.Name = "box3";
+            this.box3.ReadOnly = true;
+            this.box3.Size = new System.Drawing.Size(165, 27);
+            this.box3.TabIndex = 3;
+            this.box3.TabStop = false;
+            // 
+            // box2
+            // 
+            this.box2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.box2.Location = new System.Drawing.Point(536, 106);
+            this.box2.Margin = new System.Windows.Forms.Padding(4);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(165, 27);
+            this.box2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(311, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Divident";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(311, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Divisor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(311, 167);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Remainder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 333);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MaskedTextBox box1;
+        private System.Windows.Forms.MaskedTextBox box3;
+        private System.Windows.Forms.MaskedTextBox box2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Project A and B - KADAM/Kadam_project_3A/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class startwithtwo : Exception
+        {
+
+
+            public startwithtwo()
+            {
+                MessageBox.Show("An exception has occured the number cannot start with 2 for the second number");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string a = box2.Text;
+                decimal Divident = Convert.ToDecimal(box1.Text);
+                decimal Divisor = Convert.ToDecimal(box2.Text);
+                decimal Remainder = Divident / Divisor;
+                if (Divisor > 800)
+                {
+                    throw new OverflowException();
+                };
+                if (a.StartsWith("2"))
+                {
+                    throw new startwithtwo();
+                }
+                box3.Text = Remainder.ToString();
+                box1.Focus();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Format Exception has occured enter values in numbers only");
+                box1.Focus();
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Overflow exception has occured enter Number less than 800");
+                box1.Focus();
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Divide by zero exception has occured enter a number greater than zero for Second Number");
+                box2.Focus();
+            }
+            catch (startwithtwo)
+            {
+                box1.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
